5171a0a2d4f320c4f9dd2c91eb47b047c886fb81
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-11-09 08:18:08 -0600

    Update Main.py
    se agregaron dos funciones de euclides

--- Main.py
+++ Main.py
@@ -46,6 +46,37 @@ def primo(num): #funcion para verificar Num primos
         
     return True
 
+#algoritmo de euclides para el mcd
+def mcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+#algoritmo de euclides extendido para encontrar el inverso multiplicativo de dos numeros
+def inverso_multi(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+    
+    while e > 0:
+        temp1 = temp_phi/e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+        
+        x = x2- temp1* x1
+        y = d - temp1 * y1
+        
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+    
+    if temp_phi == 1:
+        return d + phi
+
 #funcion que encripta un mensaje
 def encriptar():
     return 1
